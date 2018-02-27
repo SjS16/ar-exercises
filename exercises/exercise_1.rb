8aaed7239d939f235a658a2289c7c6a1096c73ea
Stephanie Simpson <stephsimpson911@gmail.com>
2018-02-27 19:42:25 +0000

    finished first 2 exercises

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,16 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+burnaby = Store.create
+burnaby.annual_revenue = 300_000
+burnaby.mens_apparel = true
+burnaby.womens_apparel = true
+
+richmond = Store.create
+richmond.annual_revenue = 1_260_000
+richmond.womens_apparel = true
+
+gastown = Store.create
+gastown.annual_revenue = 1_900_000
+gastown.mens_apparel = true
+
