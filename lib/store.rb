14abe3fe64fef65b1baffb927562e3b60171a8d4
Stephanie Simpson <stephsimpson911@gmail.com>
2018-02-27 21:55:54 +0000

    did last stretch part

--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,13 @@
 class Store < ActiveRecord::Base
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than: 0}
+  validate :must_have_one_apparel_gender?
   has_many :employees
+
+  def must_have_one_apparel_gender?
+    if mens_apparel === nil && womens_apparel === nil
+      errors.add(:mens_apparel,  "Must have one apparel type.")
+    end
+  end
+
 end
