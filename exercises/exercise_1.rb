2f77bcc1c83784984f40e68df49f6e1fd41d2982
Stephanie Simpson <stephsimpson911@gmail.com>
2018-02-27 20:39:21 +0000

    first four exercises

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,16 +3,22 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-burnaby = Store.create
-burnaby.annual_revenue = 300_000
-burnaby.mens_apparel = true
-burnaby.womens_apparel = true
+burnaby = Store.create(
+name: 'burnaby',
+annual_revenue: 300_000,
+mens_apparel: true,
+womens_apparel: true)
 
-richmond = Store.create
-richmond.annual_revenue = 1_260_000
-richmond.womens_apparel = true
+richmond = Store.create(
+name: 'richmond',
+annual_revenue: 1_260_000,
+mens_apparel: false,
+womens_apparel: true)
 
-gastown = Store.create
-gastown.annual_revenue = 1_900_000
-gastown.mens_apparel = true
+gastown = Store.create(
+name: 'gastown',
+annual_revenue: 1_900_000,
+mens_apparel: true,
+womens_apparel: false)
 
+puts Store.count
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,33 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+surrey = Store.create(
+name: 'surrey',
+annual_revenue:224_000,
+mens_apparel:false,
+womens_apparel:true)
+
+whistler = Store.create(
+name: 'whistler',
+annual_revenue:1_900_000,
+mens_apparel:true,
+womens_apparel:false)
+
+yaletown = Store.create(
+name: 'yaletown',
+annual_revenue:430_000,
+mens_apparel:true,
+womens_apparel:true)
+
+puts Store.count
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |menshop|
+  puts "Shop name: #{menshop.name} has annual revenue of #{menshop.annual_revenue}"
+end
+
+@womens_stores = Store.where("annual_revenue < 1000000").where(womens_apparel: true)
+
+@womens_stores.each do |womenshop|
+  puts "Shop name: #{womenshop.name} has annual revenue of #{womenshop.annual_revenue}"
+end
